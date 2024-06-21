09180e81c51b242c8e8a67561335b76f5bfe2a95
macarena <macakiven@gmail.com>
2024-06-21 01:05:36 -0300

    feat: add projects section

--- src/app/views/home-view.component.ts
+++ src/app/views/home-view.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/HeaderComponent/header.component'
+import { ProjectsComponent } from '../components/ProjectsComponent/projects.component'
+
 @Component({
   selector: 'app-home-view',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    ProjectsComponent
   ],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.scss'
